fc858218718ccbcc2494a8d5636ee3197bbd453e
EngArchMubarak <212436050+EngArchMubarak@users.noreply.github.com>
2025-06-15 08:47:27 +0300

    lets the shams shine plz

--- shamsai_project_complete/shamsapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SimpleForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)
--- shamsai_project_complete_FULL/shamsai/settings.py
+++ shamsai_project_complete_FULL/shamsai/settings.py
@@ -1,12 +1,9 @@
-import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-secret-key'
-
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -61,7 +58,5 @@ AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
