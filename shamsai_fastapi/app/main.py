d68be3510643f8c90f3751dfd9c90cfab9a46106
EngArchMubarak <212436050+EngArchMubarak@users.noreply.github.com>
2025-06-15 09:13:29 +0300

    edit main

--- shamsai_fastapi/app/main.py
+++ shamsai_fastapi/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api import upload
 
 app = FastAPI()
 
-app.include_router(upload.router)
+@app.get("/")
+def read_root():
+    return {"message": "ShamsAI FastAPI backend is running ✅"}
